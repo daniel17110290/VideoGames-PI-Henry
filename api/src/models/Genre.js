4c89462195b89683711f85e819fee94032f47f09
Marco Galvan <elmacro11@gmail.com>
2021-09-27 15:18:13 -0300

    Modelos casi terminados

---
+++ api/src/models/Genre.js
@@ -0,0 +1,10 @@
+const { DataTypes, Sequelize } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("genre", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,10 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize, NOW } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    release_date: {
+      type: DataTypes.DATEONLY,
+      defaultValue: NOW,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      defaultValue: 3,
+    },
+    plataforms: {
       type: DataTypes.STRING,
       allowNull: false,
     },
